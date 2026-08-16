73bbb0ae4e2bb0d0dd96f3847cbee1799a765aa5
Ayush jha <jha101187@gmail.com>
2026-08-16 15:15:27 +0530

    Update videos.js

--- videos.js
+++ videos.js
@@ -1,6 +1,6 @@
 const VIDEOS = [
   {
-    url: "https://youtu.be/A3AdQ0Pnbak",
+    url: "https://fffast.filesdl.in/zdownload.php?id=CYtUVUUh0_0Mebqy8AfzCJkPmOAL0sgOqu80BQCY4k7fY3TE-bHHtRQr0dQfknOmABSYmurQ4kIhdpfRhIBy_KlrcD204l2r2sEXZBEf6HIXy1g8v_xiHFVeG_KVxph8HtITYWNE2CTv2sAJBir1jRlQVwKxoHElK8EWb8t8E4pg65J0dekYmcu_6Vjfne3GoMaQWGb0v9iKi1GcqIosuZeB7qaVPTYlcgWosLv7Lc8TleX1bWWqOaSXJIqUPcwl2o_qZlttadRFGkJURfH-krC4W-1HXq8zNYDBqLufPaBQBA&token=MTc4Njg3NjExMy44OTg3YTA4MzViZWYxNzJhZDUyYjY0NGNmZDJiNjAyODk4OThjNmMyOWYyYWNjNjdlNDliYzE2OGY3ODNkZWI3",
     title: "Ayush jha",
     category: "technology",
     date: "2026-08-16"
