e2cc48de7d5fdb2533a5b4f8a34715c790de57f7
Ayush jha <jha101187@gmail.com>
2026-08-16 15:21:45 +0530

    Update videos.js

--- videos.js
+++ videos.js
@@ -1,8 +1,8 @@
 const VIDEOS = [
   {
     url: "https://fffast.filesdl.in/zdownload.php?id=CYtUVUUh0_0Mebqy8AfzCJkPmOAL0sgOqu80BQCY4k7fY3TE-bHHtRQr0dQfknOmABSYmurQ4kIhdpfRhIBy_KlrcD204l2r2sEXZBEf6HIXy1g8v_xiHFVeG_KVxph8HtITYWNE2CTv2sAJBir1jRlQVwKxoHElK8EWb8t8E4pg65J0dekYmcu_6Vjfne3GoMaQWGb0v9iKi1GcqIosuZeB7qaVPTYlcgWosLv7Lc8TleX1bWWqOaSXJIqUPcwl2o_qZlttadRFGkJURfH-krC4W-1HXq8zNYDBqLufPaBQBA&token=MTc4Njg3NjExMy44OTg3YTA4MzViZWYxNzJhZDUyYjY0NGNmZDJiNjAyODk4OThjNmMyOWYyYWNjNjdlNDliYzE2OGY3ODNkZWI3",
-    title: "Mera Video",
-    category: "Entertainment",
+    title: "SATLUJ FULL MOVIE 720P DOWNLOAD LINK ",
+    category: "MOVIE",
     date: "2026-08-16"
   },
   {
