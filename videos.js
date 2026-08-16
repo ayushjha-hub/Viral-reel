9c18156df28bb8f1d224c1d7b168e2abbf1baa8e
Ayush jha <jha101187@gmail.com>
2026-08-16 15:31:04 +0530

    Update videos.js

--- videos.js
+++ videos.js
@@ -6,10 +6,10 @@ const VIDEOS = [
     date: "2026-08-14"
   },
   {
-    url: "https://www.youtube.com/watch?v=jNQXAC9IVRw",
-    title: "Doosra Sample Video",
-    category: "Tech",
-    date: "2026-08-14"
+    url: "https://fffast.filesdl.in/zdownload.php?id=H0AiZhYoK8vcCIRstDRZcikWUwTwAIqgFQsMkPYMIZWENlW2Lzh5gvbOx9da_b61zVnYz0Jf0wTc--zmdESxtd-7PcyEbfV5VIRW2etHjJ_K-_afn7O7rLiiMTaYdaxt9NPH0Eo9by1DJzAVKoBtwZtqQkj1dpSScjToYaxZolkSObGyYOyo-GL0miZozaAg4Q73OXhLCKPg7OQFNGdvMTXbW5Ba3IwDIEzQw2l7ig&token=MTc4Njg4ODIwMS41MmMxZDU5YzAzZjFmOWNlODkwZmMzOGFhNmM4ZjVmZWIyZmNkYjA0ZmM0MjlhNjUwMDljNWZkMWU0YjY3YjBm",
+    title: "BATWARA FULL MOVIE HALL RECORDING 720P",
+    category: "MOVIE",
+    date: "2026-08-15"
   },
   {
     url: "https://vimeo.com/76979871",
