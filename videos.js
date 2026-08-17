448f570888b0729488b68142ebcec72d7c3fd144
Ayush jha <jha101187@gmail.com>
2026-08-17 16:28:18 +0530

    Update videos.js

--- videos.js
+++ videos.js
@@ -6,7 +6,7 @@ const VIDEOS = [
     date: "2026-08-14"
   },
   {
-    url: "https://fffast.filesdl.in/zdownload.php?id=H0AiZhYoK8vcCIRstDRZcikWUwTwAIqgFQsMkPYMIZWENlW2Lzh5gvbOx9da_b61zVnYz0Jf0wTc--zmdESxtd-7PcyEbfV5VIRW2etHjJ_K-_afn7O7rLiiMTaYdaxt9NPH0Eo9by1DJzAVKoBtwZtqQkj1dpSScjToYaxZolkSObGyYOyo-GL0miZozaAg4Q73OXhLCKPg7OQFNGdvMTXbW5Ba3IwDIEzQw2l7ig&token=MTc4Njg4ODIwMS41MmMxZDU5YzAzZjFmOWNlODkwZmMzOGFhNmM4ZjVmZWIyZmNkYjA0ZmM0MjlhNjUwMDljNWZkMWU0YjY3YjBm",
+    url: "https://new6.filesdl.top/cloud/yM0Qdon6Am",
     title: "BATWARA FULL MOVIE HALL RECORDING 720P",
     category: "MOVIE",
     date: "2026-08-15"
